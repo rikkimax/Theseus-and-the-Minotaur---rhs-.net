5c6ddedf5ae4702cb60e9614c191536545edf3d4
chillNZ <nzl.kyle@gmail.com>
2013-09-26 11:59:07 +1200

    Added Classes

---
+++ Shared code base/models/Border.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Border
+    {
+	    bool right;
+	    bool down;
+        public Border(bool theRight, bool theDown)
+        {
+            this.right = theRight;
+            this.down = theDown;
+        }
+    }
+}
