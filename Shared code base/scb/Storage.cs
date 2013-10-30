1efb9791815d0ca4f61fcb12c92335ce0a1a8b63
Richard Cattermole <alphaglosined@gmail.com>
2013-10-31 10:13:27 +1300

    Abstracting out the game storage.

---
+++ Shared code base/scb/Storage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TATM.SCB.models;
+
+namespace TATM.SCB.scb
+{
+    class Storage
+    {
+        public static GameSettings settings { get; set; }
+
+        public static bool Save(/*GameSettings gs*/)
+        {
+            System.Xml.Serialization.XmlSerializer x = new System.Xml.Serialization.XmlSerializer(gs.GetType());
+            x.Serialize(Console.Out, gs.GetType());
+            System.IO.StreamWriter file = new System.IO.StreamWriter(
+    @"%APPDATA%/TATMRHS/data.xml");
+            x.Serialize(file, gs);
+            file.Close();
+            return true;
+        }
+
+        public static GameSettings Load()
+        {
+            return null;
+        }
+    }
+}
--- Shared gui code base/sgcb/GameBoardCtrl.xaml.cs
+++ Shared gui code base/sgcb/GameBoardCtrl.xaml.cs
@@ -35,15 +35,6 @@ namespace TATM.SGCB
             cells = new List<CellCtrl>();
             entities = new Dictionary<EntityType, Point>();
         }
-        public bool Save (GameSettings gs) {
-            System.Xml.Serialization.XmlSerializer x = new System.Xml.Serialization.XmlSerializer(gs.GetType()); 
-            x.Serialize(Console.Out, gs.GetType());
-            System.IO.StreamWriter file = new System.IO.StreamWriter(
-    @"%APPDATA%/TATMRHS/data.xml");
-            x.Serialize(file, gs);
-            file.Close();
-            return true;
-        }
 
         /*
          * TODO Make sure to call this before usage!
