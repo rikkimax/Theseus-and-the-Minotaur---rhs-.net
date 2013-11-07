37ea0c6c8be04ed2ad832a18cbcf13da41dfc10c
Richard Cattermole <alphaglosined@gmail.com>
2013-11-07 14:56:16 +1300

    Moving default load data into Load method of Storage.

--- Shared code base/scb/Storage.cs
+++ Shared code base/scb/Storage.cs
@@ -67,6 +67,70 @@ namespace TATM.SCB
             {
                 settings = null;
             }
+
+            if (Storage.settings == null || Storage.settings.maps.Count == 0)
+            {
+                settings = new GameSettings();
+
+                GameBoard board = new GameBoard();
+                board.level = 0;
+                board.minotaur = new Entity(1, 0);
+                board.theseus = new Entity(1, 2);
+                board.width = 3;
+                board.height = 3;
+                board.cells = new List<Cell>();
+
+                board.cells.Add(new Cell(0, 0, new Border(false, false), false, false));
+                board.cells.Add(new Cell(1, 0, new Border(false, true), false, false));
+                board.cells.Add(new Cell(2, 0, new Border(true, false), false, false));
+
+                board.cells.Add(new Cell(0, 1, new Border(false, false), false, false));
+                board.cells.Add(new Cell(1, 1, new Border(true, true), false, false));
+                board.cells.Add(new Cell(2, 1, new Border(true, false), true, false));
+
+                board.cells.Add(new Cell(0, 2, new Border(false, true), false, false));
+                board.cells.Add(new Cell(1, 2, new Border(false, true), false, false));
+                board.cells.Add(new Cell(2, 2, new Border(true, true), false, false));
+
+                settings.maps.Add(board);
+
+                board = new GameBoard();
+                board.level = 1;
+                board.minotaur = new Entity(1, 2);
+                board.theseus = new Entity(1, 0);
+                board.width = 3;
+                board.height = 3;
+                board.cells = new List<Cell>();
+
+                board.cells.Add(new Cell(0, 0, new Border(false, false), false, false));
+                board.cells.Add(new Cell(1, 0, new Border(false, true), false, false));
+                board.cells.Add(new Cell(2, 0, new Border(true, false), false, false));
+
+                board.cells.Add(new Cell(0, 1, new Border(false, false), false, false));
+                board.cells.Add(new Cell(1, 1, new Border(true, true), false, false));
+                board.cells.Add(new Cell(2, 1, new Border(true, false), true, false));
+
+                board.cells.Add(new Cell(0, 2, new Border(false, true), false, false));
+                board.cells.Add(new Cell(1, 2, new Border(false, true), false, false));
+                board.cells.Add(new Cell(2, 2, new Border(true, true), false, false));
+
+                settings.maps.Add(board);
+
+                Player player = new Player();
+                player.name = Environment.UserName;
+                settings.players.Add(player);
+
+                Storage.settings = settings;
+                Storage.currentPlayer = player;
+
+                Storage.Save();
+            }
+
+            Storage.currentPlayer = Storage.settings.players.Find(delegate(Player player)
+            {
+                return player.name == Environment.UserName;
+            });
+
             return settings;
         }
 
